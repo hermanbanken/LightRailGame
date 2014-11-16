a59dc758a8020d8c1827a5d8911385fd7f391bd1
Herman Banken <hermanbanken@gmail.com>
2014-11-16 14:50:24 +0100

    Simple Graph navigation sample

---
+++ Dummy/Assets/Node.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Node : MonoBehaviour {
+
+	public GameObject nextNode;
+	private LineRenderer line;
+	private Vector3 from;
+	private Vector3 to;
+
+	// Use this for initialization
+	void Start () {
+		from = this.transform.position;
+		if (nextNode != null)
+			to = nextNode.transform.position;
+		else
+			to = from;
+
+		CreateLine ();
+	}
+
+	void CreateLine(){
+		from = Vector3.zero;
+		to = Vector3.zero;
+
+		line = gameObject.GetComponent<LineRenderer> ();
+		if (line == null) {
+			line = gameObject.AddComponent("LineRenderer") as LineRenderer;
+		}
+		line.SetVertexCount (nextNode == null ? 1 : 2);
+
+		line.SetColors (new Color(255,0,0,100), new Color(0,255,0,100));
+		line.SetVertexCount (2);
+		line.SetWidth(0.3f, 0.3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (line == null || !line.isVisible) {
+			CreateLine ();
+		}
+
+		if (from != this.transform.position) {
+			from = this.transform.position;
+			line.SetPosition(0, from);
+		}
+		if (nextNode != null && to != nextNode.transform.position) {
+			to = nextNode.transform.position;
+			line.SetPosition(1, to);
+		}
+	}
+
+	public Node PositionOnLineReturningOverflow(float unitsFromStart, out float newUnitsFromStart, out Vector3 position, out Vector3 destination){
+		position = to;
+		destination = to;
+
+		if (to == from) {
+			newUnitsFromStart = 0;
+			return this;
+		}
+
+		float overflow = unitsFromStart - (to - from).magnitude;
+
+		if (overflow > 0 && nextNode != null && nextNode.GetComponent<Node> () != null) {
+			return nextNode.GetComponent<Node> ().PositionOnLineReturningOverflow (overflow, out newUnitsFromStart, out position, out destination);
+		} else if (overflow > 0) {
+			position = to;
+			newUnitsFromStart = (to - from).magnitude;
+			return this;
+		}
+
+		newUnitsFromStart = unitsFromStart;
+		position = from + (to - from).normalized * unitsFromStart;
+		return this;
+	}
+	
+}
---
+++ Dummy/Assets/Train.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Train : MonoBehaviour {
+
+	public GameObject StartingNode;
+	private Node currentNode;
+	public bool forward = true;
+	public float speed = 10f;
+	private float position = 0f;
+
+	// Use this for initialization
+	void Start () {
+		if (StartingNode == null)
+			return;
+
+		currentNode = StartingNode.GetComponent<Node>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentNode == null)
+			return;
+		
+		position += speed * Time.deltaTime;
+
+		Vector3 pos;
+		Vector3 dest;
+		currentNode = currentNode.PositionOnLineReturningOverflow (position, out position, out pos, out dest);
+		transform.position = pos;
+		transform.rotation = Quaternion.LookRotation (dest - pos); ///Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(dest - pos));
+	}
+}
