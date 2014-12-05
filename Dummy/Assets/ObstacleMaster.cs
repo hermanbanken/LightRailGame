5bfa11596cff46a222ca6953de855dd31daf1b7e
bluepig <ysstwins@gmail.com>
2014-12-05 16:35:37 +0100

    Set position of Obstacle button

--- Dummy/Assets/ObstacleMaster.cs
+++ Dummy/Assets/ObstacleMaster.cs
@@ -28,15 +28,32 @@ public class ObstacleMaster : MonoBehaviour {
 	void running (){
 		// Get random position
 		Edge edge = game.graph.edges.ElementAt(rnd.Next(0, game.graph.edges.Count ()-1));
-		Vector3 pos = edge.GetPoint ((float)rnd.NextDouble ());
+		float randT = (float)rnd.NextDouble ();
+		Vector3 pos = edge.GetPoint (randT);
+		Vector3 dir = edge.GetDirection (randT);
+		Vector3 buttonPosition = getButtonPosition (pos, dir, 7);
 
 		Obstacle obstacle = new GameObject ().AddComponent<Obstacle> ();
+	
 		obstacle.init(pos, ObstacleType.Car, onUserActioned);
+		obstacle.button.transform.localScale = new Vector3 (9, 3, 1);
+		obstacle.button.transform.position = buttonPosition;
 		obstacles.Add (obstacle);
 		obstaclesPos.Add (obstacle.block.transform.position);
 		if(onOccur != null) onOccur (obstacle);
 	}
+	
 
+	Vector3 getButtonPosition(Vector3 pos, Vector3 dir, float distance){
+		Vector3 a = new Vector3 ();
+		Vector3 b = new Vector3 ();
+		a.x = pos.x + distance * dir.y / (float)Math.Sqrt (dir.x * dir.x + dir.y * dir.y);
+		a.y = pos.y - distance * dir.x / (float)Math.Sqrt (dir.x * dir.x + dir.y * dir.y);
+		b.x = pos.x - distance * dir.y / (float)Math.Sqrt (dir.x * dir.x + dir.y * dir.y);
+		b.y = pos.y + distance * dir.x / (float)Math.Sqrt (dir.x * dir.x + dir.y * dir.y);
+		a.z = b.z = -4;
+		return (a.magnitude < b.magnitude) ? a : b;
+		}
 	void Update (){
 		// Tick
 		obstacles.ForEach(p => { if (p != null) p.Tick(); });
