29a1a91852286c549097d42f495bbc02b973a385
rendongliu <rendong_liu@hotmail.com>
2014-12-08 12:18:07 +0200

    Implement basic panning & zooming

--- Dummy/Assets/Mouse.cs
+++ Dummy/Assets/Mouse.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class Mouse {
 
 	private bool down = false;
 	public Queue<MouseEvent> Events = new Queue<MouseEvent>();
+	private MouseEvent Last;
 
 	public Mouse(){
 	}
 
 	public void OnFrame(){
+		// Still down
+		if (down && Input.GetMouseButton (0)) {
+			Last.HandleDrag(Input.mousePosition);
+		}
+
 		// New click
 		if (!down && Input.GetMouseButtonDown (0)) {
-			Events.Enqueue(new MouseClick(Input.mousePosition));
+			Last = new MouseClick(Input.mousePosition);
+			Events.Enqueue(Last);
 			down = true;
 			//ScoreManager.score++;
 		}
@@ -30,6 +38,11 @@ public abstract class MouseEvent {
 	public readonly Vector3 position;
 	public MouseEvent(Vector3 position){
 		this.position = position;
+	}
+
+	public event Action<Vector3> OnDrag;
+	public void HandleDrag(Vector3 newPosition){
+		OnDrag (newPosition);
 	}
 }
 
