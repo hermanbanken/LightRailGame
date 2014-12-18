9887ddd8bd4236d6527b764a081e4099895ccf19
rendongliu <rendong_liu@hotmail.com>
2014-12-18 17:00:49 +0200

    Missing files

---
+++ Dummy/Assets/Mapchoose.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Mapchoose : MonoBehaviour {
+
+	public void ChooseMap(string name){
+		Application.LoadLevel (name);
+	}
+
+}
