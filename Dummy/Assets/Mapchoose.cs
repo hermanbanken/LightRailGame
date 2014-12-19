7d2af6867f8eae48bca2dcf5e690829c77480236
rendongliu <rendong_liu@hotmail.com>
2014-12-19 17:29:06 +0200

    Get the tutorial

--- Dummy/Assets/Mapchoose.cs
+++ Dummy/Assets/Mapchoose.cs
@@ -5,11 +5,7 @@ using UnityEngine.UI;
 public class Mapchoose : MonoBehaviour {
 
 	public void ChooseMap(string name){
-		Debug.Log ("Loading " + name);
-		if (name.Equals ("TutorialLevel")) {
-			Debug.Log ("Difficulty = 0");
-			LightRailGame.Difficulty = -1;
-		}
+
 		Application.LoadLevel (name);
 
 	}
