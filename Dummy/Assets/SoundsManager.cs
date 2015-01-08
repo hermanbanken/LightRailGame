ea7f66dc60b735ad6c57dccb19b31237d6dcc975
rendongliu <rendong_liu@hotmail.com>
2015-01-08 16:26:24 +0200

    already add the sound effect of the game

---
+++ Dummy/Assets/SoundsManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundsManager : MonoBehaviour {
+	public AudioClip warning;
+	public AudioClip Pickup;
+	public AudioClip Reroutefinish;
+	public AudioClip action1;
+	public AudioClip SuccRemoved;
+	public AudioClip Occur;
+	private AudioSource source;
+
+
+	void Awake () {	
+		source = GetComponent<AudioSource>();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		LightRailGame.GetInstance().OnSelectedGameObjectChanged += (GameObject obj) => {
+			// The user selected something
+			if(obj == null)
+				return;
+			if(obj.GetComponent<Train>() != null) {
+				source.PlayOneShot(Pickup,0.5f);			
+			}
+		};
+
+		LightRailGame.GetInstance().OnIncidentMenuOpen += (IIncident obj) => {
+			source.PlayOneShot(warning,0.50f);
+		};
+
+		LightRailGame.ScoreManager.OnReroute += (object sender, ScoreManager.RerouteEventArgs e)=>
+		{
+			source.PlayOneShot(Reroutefinish,0.50f);
+		};
+
+		LightRailGame.ScoreManager.OnUserAction += (IIncident obj) => {
+			source.PlayOneShot(action1,0.50f);
+			int suit = obj.Suitability(obj.GetChosenSolution());
+		};
+
+		LightRailGame.ScoreManager.OnOccur += (IIncident obj) => {
+			source.PlayOneShot(Occur,0.5f);
+		};
+
+		LightRailGame.ScoreManager.OnResolved += (IIncident obj) => {
+			source.PlayOneShot(SuccRemoved,0.50f);
+		};
+
+	}
+}
